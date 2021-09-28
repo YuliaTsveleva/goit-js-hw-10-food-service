333b2bb6179fcc04cf98873e3a0034eb2466071b
YuliaTsveleva <tsveleva.yulia@gmail.com>
2021-09-29 01:15:09 +0300

    refactoring2

--- src/index.js
+++ src/index.js
@@ -2,19 +2,14 @@ import menuTemplate from '../templates/menu.hbs';
 import menu from './menu.json';
 
 const menuList = document.querySelector('.js-menu');
-
-menuList.insertAdjacentHTML('afterbegin', createMenuMarkup(menu));
-
-function createMenuMarkup(menu) {
-  return menuTemplate(menu);
-}
-
 const themeSwitcher = document.querySelector('.theme-switch__toggle');
 const body = document.querySelector('body');
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
+
+menuList.insertAdjacentHTML('afterbegin', createMenuMarkup(menu));
 
 if (localStorage.getItem('theme-swith') === 'darkTheme') {
   toSetDarkTheme();
@@ -28,6 +23,10 @@ themeSwitcher.addEventListener('change', e => {
     toSetLightTheme();
   }
 });
+
+function createMenuMarkup(menu) {
+  return menuTemplate(menu);
+}
 
 function toSetDarkTheme(e) {
   body.classList.add(Theme.DARK);
